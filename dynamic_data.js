ba351070e5ee080ae7ee3389c4c36e5ba4e87270
unknown <8caffe.1@gmail.com>
2024-05-20 18:05:21 +0330

    20 May 24

--- dynamic_data.js
+++ dynamic_data.js
@@ -1,120 +1,124 @@
 //  No JSON = "dynamic_data.js"
 const tabData = [
     { title: 'گرم بر پایه اسپرسو', id: "1", color: 'red', items: [
-        { title: 'ESPRESSO •',name: "اسپرسو", image: 'img/menu-item-11.jpg', price: '52~72(100 عربیکا)' },
-        { title: 'AMERIKANO •••',name: "دبل اسپرسو+آب", image: 'img/menu-item-12.jpg', price: '54~74(100 عربیکا)' },
-        { title: 'ESPRESSO MACCHIATO •',name: "دبل اسپرسو+فوم شیر", image: 'img/menu-item-13.jpg', price: '60~78(100 عربیکا)' },
-        { title: 'ESPRESSO CON PANNA •',name: "دبل اسپرسو+خامه", image: 'img/menu-item-14.jpg', price: '64~84(100 عربیکا)' },
-        { title: 'CAPPUCCINO ••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-15.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'CORTADO ••',name: "دبل اسپرسو+شیر با دمای ۶۰ درجه", image: 'img/menu-item-16.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'FLAT WHITE ••',name: "دبل اسپرسو+شیر بدون فوم", image: 'img/menu-item-17.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'LATTE •••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-18.jpg', price: '72~90(100 عربیکا)' },
-        { title: 'CARAMEL MACCHIATO •••',name: "دبل اسپرسو+شیر+کارامل", image: 'img/menu-item-19.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'MOCHA •••',name: "دبل اسپرسو+شیر+شکلات", image: 'img/menu-item-110.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'PINK LATTE •••',name: "دبل اسپرسو+شیر+چغندر", image: 'img/menu-item-111.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'AFFOGATO •••',name: "دبل اسپرسو+بستنی", image: 'img/menu-item-112.jpg', price: '78~96(100 عربیکا)' }
+        { title: 'ESPRESSO •',name: "اسپرسو", image: 'img/menu-item-0101.jpg', price: '52~72(100 عربیکا)' },
+        { title: 'AMERIKANO •••',name: "دبل اسپرسو+آب", image: 'img/menu-item-0102.jpg', price: '54~74(100 عربیکا)' },
+        { title: 'ESPRESSO MACCHIATO •',name: "دبل اسپرسو+فوم شیر", image: 'img/menu-item-0103.jpg', price: '60~78(100 عربیکا)' },
+        { title: 'ESPRESSO CON PANNA •',name: "دبل اسپرسو+خامه", image: 'img/menu-item-0104.jpg', price: '64~84(100 عربیکا)' },
+        { title: 'CAPPUCCINO ••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-0105.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'CORTADO ••',name: "دبل اسپرسو+شیر با دمای ۶۰ درجه", image: 'img/menu-item-0106.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'FLAT WHITE ••',name: "دبل اسپرسو+شیر بدون فوم", image: 'img/menu-item-0107.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'LATTE •••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-0108.jpg', price: '72~90(100 عربیکا)' },
+        { title: 'CARAMEL MACCHIATO •••',name: "دبل اسپرسو+شیر+کارامل", image: 'img/menu-item-0109.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'MOCHA •••',name: "دبل اسپرسو+شیر+شکلات", image: 'img/menu-item-0110.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'PINK LATTE •••',name: "دبل اسپرسو+شیر+چغندر", image: 'img/menu-item-0111.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'AFFOGATO •••',name: "دبل اسپرسو+بستنی", image: 'img/menu-item-0112.jpg', price: '78~96(100 عربیکا)' },
+
+
+        
     
         // Add more menu items for this tab if needed
     ] },
     { title: 'سرد بر پایه اسپرسو', id: "2", color: 'green', items: [
-        { title: 'Cold AMERIKANO •••',name: "دبل اسپرسو+آب", image: 'img/menu-item-21.jpg', price: '54~74(100 عربیکا)' },
-        { title: 'Cold ESPRESSO MACCHIATO •',name: "دبل اسپرسو+فوم شیر", image: 'img/menu-item-22.jpg', price: '60~78(100 عربیکا)' },
-        { title: 'Cold ESPRESSO CON PANNA •',name: "دبل اسپرسو+خامه", image: 'img/menu-item-23.jpg', price: '64~84(100 عربیکا)' },
-        { title: 'Cold CAPPUCCINO ••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-24.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'Cold CORTADO ••',name: "دبل اسپرسو+شیر با دمای ۶۰ درجه", image: 'img/menu-item-25.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'Cold FLAT WHITE ••',name: "دبل اسپرسو+شیر بدون فوم", image: 'img/menu-item-26.jpg', price: '66~86(100 عربیکا)' },
-        { title: 'Cold LATTE •••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-27.jpg', price: '72~90(100 عربیکا)' },
-        { title: 'Cold CARAMEL MACCHIATO •••',name: "دبل اسپرسو+شیر+کارامل", image: 'img/menu-item-28.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'Cold MOCHA •••',name: "دبل اسپرسو+شیر+شکلات", image: 'img/menu-item-29.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'Cold PINK LATTE •••',name: "دبل اسپرسو+شیر+چغندر", image: 'img/menu-item-210.jpg', price: '78~96(100 عربیکا)' },
-        { title: 'Cold AFFOGATO •••',name: "دبل اسپرسو+بستنی", image: 'img/menu-item-211.jpg', price: '78~96(100 عربیکا)' }
+        { title: 'Cold AMERIKANO •••',name: "دبل اسپرسو+آب", image: 'img/menu-item-0201.jpg', price: '54~74(100 عربیکا)' },
+        { title: 'Cold ESPRESSO MACCHIATO •',name: "دبل اسپرسو+فوم شیر", image: 'img/menu-item-0202.jpg', price: '60~78(100 عربیکا)' },
+        { title: 'Cold ESPRESSO CON PANNA •',name: "دبل اسپرسو+خامه", image: 'img/menu-item-0203.jpg', price: '64~84(100 عربیکا)' },
+        { title: 'Cold CAPPUCCINO ••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-0204.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'Cold CORTADO ••',name: "دبل اسپرسو+شیر با دمای ۶۰ درجه", image: 'img/menu-item-0205.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'Cold FLAT WHITE ••',name: "دبل اسپرسو+شیر بدون فوم", image: 'img/menu-item-0206.jpg', price: '66~86(100 عربیکا)' },
+        { title: 'Cold LATTE •••',name: "دبل اسپرسو+شیر", image: 'img/menu-item-0207.jpg', price: '72~90(100 عربیکا)' },
+        { title: 'Cold CARAMEL MACCHIATO •••',name: "دبل اسپرسو+شیر+کارامل", image: 'img/menu-item-0208.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'Cold MOCHA •••',name: "دبل اسپرسو+شیر+شکلات", image: 'img/menu-item-0209.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'Cold PINK LATTE •••',name: "دبل اسپرسو+شیر+چغندر", image: 'img/menu-item-0210.jpg', price: '78~96(100 عربیکا)' },
+        { title: 'Cold AFFOGATO •••',name: "دبل اسپرسو+بستنی", image: 'img/menu-item-0211.jpg', price: '78~96(100 عربیکا)' }
         // Add menu items for this tab if needed
     ] },
     { title: 'نوشیدنی های گرم', id: "3", color: 'blue', items: [
-        { title: 'MASALA TEA •••',name: "چای ماسالا", image: 'img/menu-item-31.jpg', price: '70' },
-        { title: 'HOT CHCOLATE •••',name: "هات چاکلت", image: 'img/menu-item-32.jpg', price: '70' },
-        { title: 'KARAK TEA •••',name: "چای+شیر+هل+میخک+گردو", image: 'img/menu-item-33.jpg', price: '78' },
-        { title: 'MATCHA LATTE •••',name: "ماچا+شیر", image: 'img/menu-item-34.jpg', price: '78' }
+        { title: 'MASALA TEA •••',name: "چای ماسالا", image: 'img/menu-item-0301.jpg', price: '70' },
+        { title: 'HOT CHCOLATE •••',name: "هات چاکلت", image: 'img/menu-item-0302.jpg', price: '70' },
+        { title: 'KARAK TEA •••',name: "چای+شیر+هل+میخک+گردو", image: 'img/menu-item-0303.jpg', price: '78' },
+        { title: 'MATCHA LATTE •••',name: "ماچا+شیر", image: 'img/menu-item-0304.jpg', price: '78' }
         // Add menu items for this tab if needed
     ] },
     { title: 'نوشیدنی های جدید', id: "4", color: 'orange', items: [
-        { title: 'EIGHT MINUTS •••',name: "دبل اسپرسو+شیربادام+نارگیل+وانیل", image: 'img/menu-item-41.jpg', price: '88~108(100 عربیکا)' },
-        { title: 'CINNAMON LATTE •••',name: "دبل اسپرسو+شیر+دارچین+عسل", image: 'img/menu-item-42.jpg', price: '88~108(100 عربیکا)' }
+        { title: 'EIGHT MINUTS •••',name: "دبل اسپرسو+شیربادام+نارگیل+وانیل", image: 'img/menu-item-0401.jpg', price: '88~108(100 عربیکا)' },
+        { title: 'CINNAMON LATTE •••',name: "دبل اسپرسو+شیر+دارچین+عسل", image: 'img/menu-item-0402.jpg', price: '88~108(100 عربیکا)' }
         // Add menu items for this tab if needed
     ] },
     { title: 'چای و دمنوش', id: "5", color: 'cyan', items: [
-        { title: 'چای',name: "", image: 'img/menu-item-51.jpg', price: '40' },
-        { title: "چای زعفران",name: "", image: 'img/menu-item-52.jpg', price: '55' },
-        { title: 'دم غروب',name: "چای ترش+نعنا+چای کوهی", image: 'img/menu-item-53.jpg', price: '55' },
-        { title: 'شب زمستون',name: "آویشن+به لیمو+عسل", image: 'img/menu-item-54.jpg', price: '55' },
-        { title: 'هشت دقیقه',name: "به+گل سرخ+بهارنارنج+به لیمو", image: 'img/menu-item-55.jpg', price: '55' },
-        { title: 'دمنوش آرامش',name: "گل گاوزبان+بهارنارنج+به لیمو", image: 'img/menu-item-56.jpg', price: '55' },
-        { title: 'چای اولانگ',name: "چای سبز تخمیری", image: 'img/menu-item-57.jpg', price: '65' },
-        { title: 'چای تک برگ دارجلینگ',name: "چای شامپاین سیاه از هندوستان", image: 'img/menu-item-58.jpg', price: '65' },
-        { title: 'چای سبز',name: "", image: 'img/menu-item-59.jpg', price: '45' }
+        { title: 'چای',name: "", image: 'img/menu-item-0501.jpg', price: '40' },
+        { title: "چای زعفران",name: "", image: 'img/menu-item-0502.jpg', price: '55' },
+        { title: 'دم غروب',name: "چای ترش+نعنا+چای کوهی", image: 'img/menu-item-0503.jpg', price: '55' },
+        { title: 'شب زمستون',name: "آویشن+به لیمو+عسل", image: 'img/menu-item-0504.jpg', price: '55' },
+        { title: 'هشت دقیقه',name: "به+گل سرخ+بهارنارنج+به لیمو", image: 'img/menu-item-0505.jpg', price: '55' },
+        { title: 'دمنوش آرامش',name: "گل گاوزبان+بهارنارنج+به لیمو", image: 'img/menu-item-0506.jpg', price: '55' },
+        { title: 'چای اولانگ',name: "چای سبز تخمیری", image: 'img/menu-item-0507.jpg', price: '65' },
+        { title: 'چای تک برگ دارجلینگ',name: "چای شامپاین سیاه از هندوستان", image: 'img/menu-item-0508.jpg', price: '65' },
+        { title: 'چای سبز',name: "", image: 'img/menu-item-0509.jpg', price: '45' }
         // Add menu items for this tab if needed
     ] },
     { title: 'ماکتل و اسموتی', id: "6", color: 'black', items: [
-        { title: 'گادز گلاو',name: "شاه توت+توت فرنگی+انرژی زا", image: 'img/menu-item-61.jpg', price: '98' },
-        { title: 'بلوفیوژن',name: "آلوورا+لیمو+بلوکارمانو+پنیرکت", image: 'img/menu-item-62.jpg', price: '88' },
-        { title: 'طهرون',name: "زعفران+دانه چیتا+لیمو+عرقیجات", image: 'img/menu-item-63.jpg', price: '88' },
-        { title: 'موهیتو',name: "لیمو+نعنا+سودا", image: 'img/menu-item-64.jpg', price: '78' },
-        { title: 'لیموناد',name: "لیمو+سودا", image: 'img/menu-item-65.jpg', price: '68' },
-        { title: 'گرین آلگال',name: "اسپرولینا+موز+پرتقال+انبه", image: 'img/menu-item-66.jpg', price: '98' },
-        { title: 'چیا پشن',name: "هلو+پرتغال+پودینگ+دانه چیا", image: 'img/menu-item-67.jpg', price: '98' },
-        { title: 'سانست بری',name: "شاه توت+توت فرنگی+آناناس", image: 'img/menu-item-68.jpg', price: '98' }
+        { title: 'گادز گلاو',name: "شاه توت+توت فرنگی+انرژی زا", image: 'img/menu-item-0601.jpg', price: '98' },
+        { title: 'بلوفیوژن',name: "آلوورا+لیمو+بلوکارمانو+پنیرکت", image: 'img/menu-item-0602.jpg', price: '88' },
+        { title: 'طهرون',name: "زعفران+دانه چیتا+لیمو+عرقیجات", image: 'img/menu-item-0603.jpg', price: '88' },
+        { title: 'موهیتو',name: "لیمو+نعنا+سودا", image: 'img/menu-item-0604.jpg', price: '78' },
+        { title: 'لیموناد',name: "لیمو+سودا", image: 'img/menu-item-0605.jpg', price: '68' },
+        { title: 'گرین آلگال',name: "اسپرولینا+موز+پرتقال+انبه", image: 'img/menu-item-0606.jpg', price: '98' },
+        { title: 'چیا پشن',name: "هلو+پرتغال+پودینگ+دانه چیا", image: 'img/menu-item-0607.jpg', price: '98' },
+        { title: 'سانست بری',name: "شاه توت+توت فرنگی+آناناس", image: 'img/menu-item-0608.jpg', price: '98' }
+        
         // Add menu items for this tab if needed
     ] },
     { title: 'قهوه های دمی', id: "7", color: 'white', items: [
-        { title: 'TURKISH KAHWA',name: "", image: 'img/menu-item-71.jpg', price: '65' },
-        { title: 'FRENECH PRESS',name: "", image: 'img/menu-item-72.jpg', price: '85' },
-        { title: 'SIPHON',name: "", image: 'img/menu-item-73.jpg', price: '95' },
-        { title: 'CHEMEX',name: "", image: 'img/menu-item-74.jpg', price: '95' },
-        { title: 'KALITA',name: "", image: 'img/menu-item-75.jpg', price: '95' },
-        { title: 'V60',name: "", image: 'img/menu-item-76.jpg', price: '95' }
+        { title: 'TURKISH KAHWA',name: "", image: 'img/menu-item-0701.jpg', price: '65' },
+        { title: 'FRENECH PRESS',name: "", image: 'img/menu-item-0702.jpg', price: '85' },
+        { title: 'SIPHON',name: "", image: 'img/menu-item-0703.jpg', price: '95' },
+        { title: 'CHEMEX',name: "", image: 'img/menu-item-0704.jpg', price: '95' },
+        { title: 'KALITA',name: "", image: 'img/menu-item-0705.jpg', price: '95' },
+        { title: 'V60',name: "", image: 'img/menu-item-0706.jpg', price: '95' }
         // Add menu items for this tab if needed
     ] },
     { title: 'قهوه های دمی سرد', id: "8", color: 'yellow', items: [
-        { title: 'COLD BREW',name: "", image: 'img/menu-item-81.jpg', price: '85' },
-        { title: 'COLD BREW TONIK',name: "گلدبرو+تونیک واتر", image: 'img/menu-item-82.jpg', price: '90' },
-        { title: 'COLD BREW PASSION',name: "گلدبرو+تونیک واتر+لیمو+پشن فروت", image: 'img/menu-item-83.jpg', price: '105' },
-        { title: 'COLD BREW TONIK COCONUT',name: "گلدبرو+تونیک+پرتغال+نارگیل", image: 'img/menu-item-84.jpg', price: '105' }
+        { title: 'COLD BREW',name: "", image: 'img/menu-item-0801.jpg', price: '85' },
+        { title: 'COLD BREW TONIK',name: "گلدبرو+تونیک واتر", image: 'img/menu-item-0802.jpg', price: '90' },
+        { title: 'COLD BREW PASSION',name: "گلدبرو+تونیک واتر+لیمو+پشن فروت", image: 'img/menu-item-0803.jpg', price: '105' },
+        { title: 'COLD BREW TONIK COCONUT',name: "گلدبرو+تونیک+پرتغال+نارگیل", image: 'img/menu-item-0804.jpg', price: '105' }
         // Add menu items for this tab if needed
     ] },
     { title: 'شیک', id: "9", color: 'jade', items: [
-        { title: "شکلات فندق",name: "", image: 'img/menu-item-91.jpg', price: '98' },
-        { title: "بادام زمینی",name: "", image: 'img/menu-item-92.jpg', price: '98' },
-        { title: "اسپرسو",name: "" , image: 'img/menu-item-93.jpg', price: '100' },
-        { title: 'هشت دقیقه',name: "موز+نارگیل+ماسالا+اسپرسو", image: 'img/menu-item-94.jpg', price: '120' }
+        { title: "شکلات فندق",name: "", image: 'img/menu-item-0901.jpg', price: '98' },
+        { title: "بادام زمینی",name: "", image: 'img/menu-item-0902.jpg', price: '98' },
+        { title: "اسپرسو",name: "" , image: 'img/menu-item-0903.jpg', price: '100' },
+        { title: 'هشت دقیقه',name: "موز+نارگیل+ماسالا+اسپرسو", image: 'img/menu-item-0904.jpg', price: '120' }
         // Add menu items for this tab if needed
     ] },
     { title: 'دسر', id: "10", color: 'gold', items: [
-        { title: "چیز کیک",name:"" , image: 'img/menu-item-101.jpg', price: '80' },
-        { title: "رول دارچین",name: "", image: 'img/menu-item-102.jpg', price: '80' },
-        { title: "کیک روز",name: "", image: 'img/menu-item-103.jpg', price: '68' },
-        { title: "براونی",name: "", image: 'img/menu-item-104.jpg', price: '68' }
+        { title: "چیز کیک",name:"" , image: 'img/menu-item-1001.jpg', price: '80' },
+        { title: "رول دارچین",name: "", image: 'img/menu-item-1002.jpg', price: '80' },
+        { title: "کیک روز",name: "", image: 'img/menu-item-1003.jpg', price: '68' },
+        { title: "براونی",name: "", image: 'img/menu-item-1004.jpg', price: '68' }
         // Add menu items for this tab if needed
     ] },
     { title: 'میان وعده', id: "11", color: 'bronze', items: [
-        { title: 'CESAR SALAD',name: "سالاد سزار", image: 'img/menu-item-111.jpg', price: '168' },
-        { title: '',name: "وافل ساده", image: 'img/menu-item-112.jpg', price: '90' },
-        { title: '',name: "کروسان ساده", image: 'img/menu-item-113.jpg', price: '65' },
-        { title: '',name: "تست ژامبون", image: 'img/menu-item-114.jpg', price: '120' },
-        { title: '',name: "چاپاتا ژامبون", image: 'img/menu-item-115.jpg', price: '168' },
-        { title: '',name: "کروسان خیار پنیر", image: 'img/menu-item-116.jpg', price: '85' },
-        { title: '',name: "املت", image: 'img/menu-item-117.jpg', price: '70' }
+        { title: 'CESAR SALAD',name: "سالاد سزار", image: 'img/menu-item-1101.jpg', price: '168' },
+        { title: '',name: "وافل ساده", image: 'img/menu-item-1102.jpg', price: '90' },
+        { title: '',name: "کروسان ساده", image: 'img/menu-item-1103.jpg', price: '65' },
+        { title: '',name: "تست ژامبون", image: 'img/menu-item-1104.jpg', price: '120' },
+        { title: '',name: "چاپاتا ژامبون", image: 'img/menu-item-1105.jpg', price: '168' },
+        { title: '',name: "کروسان خیار پنیر", image: 'img/menu-item-1106.jpg', price: '85' },
+        { title: '',name: "املت", image: 'img/menu-item-1107.jpg', price: '70' }
         // Add menu items for this tab if needed
     ] },
     { title: 'تاپینگ', id: "12", color: 'silver', items: [
-        { title: 'SYRUP',name: "سیروپ", image: 'img/menu-item-121.jpg', price: '10' },
-        { title: 'HONEY',name: "عسل", image: 'img/menu-item-122.jpg', price: '10' },
-        { title: 'CHOCOLATE',name: "شکلات", image: 'img/menu-item-123.jpg', price: '10' },
-        { title: 'FRUITS',name: "موز+توت فرنگی", image: 'img/menu-item-124.jpg', price: '15' },
-        { title: 'ICE CREAM',name: "بستنی", image: 'img/menu-item-125.jpg', price: '15' },
-        { title: 'CREAM',name: "خامه", image: 'img/menu-item-126.jpg', price: '15' },
-        { title: 'LIME',name: "لیمو ترش", image: 'img/menu-item-127.jpg', price: '5' },
-        { title: 'CINNAMON',name: "دارچین", image: 'img/menu-item-128.jpg', price: '5' },
-        { title: 'CARDAMOM',name: "هل", image: 'img/menu-item-129.jpg', price: '5' },
+        { title: 'SYRUP',name: "سیروپ", image: 'img/menu-item-1201.jpg', price: '10' },
+        { title: 'HONEY',name: "عسل", image: 'img/menu-item-1202.jpg', price: '10' },
+        { title: 'CHOCOLATE',name: "شکلات", image: 'img/menu-item-1203.jpg', price: '10' },
+        { title: 'FRUITS',name: "موز+توت فرنگی", image: 'img/menu-item-1204.jpg', price: '15' },
+        { title: 'ICE CREAM',name: "بستنی", image: 'img/menu-item-1205.jpg', price: '15' },
+        { title: 'CREAM',name: "خامه", image: 'img/menu-item-1206.jpg', price: '15' },
+        { title: 'LIME',name: "لیمو ترش", image: 'img/menu-item-1207.jpg', price: '5' },
+        { title: 'CINNAMON',name: "دارچین", image: 'img/menu-item-1208.jpg', price: '5' },
+        { title: 'CARDAMOM',name: "هل", image: 'img/menu-item-1209.jpg', price: '5' },
         // Add menu items for this tab if needed
     ] }
 ];
